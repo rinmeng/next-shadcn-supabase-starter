3f91fe957469c4f84c47c1e195747f242048ec73
rin-windows <phurinmeng@gmail.com>
2025-11-17 21:31:03 -0800

    style: update ModeToggle icon sizes and transition durations for improved visual consistency

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -16,12 +16,12 @@ export function ModeToggle() {
   return (
     <Button variant='outline' size='icon' onClick={toggleTheme}>
       <Sun
-        className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-300
-          dark:-rotate-90 dark:scale-0'
+        className='rotate-0 scale-100 transition-all duration-500 dark:-rotate-90
+          dark:scale-0 dark:translate-x-full'
       />
       <Moon
-        className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all
-          duration-300 dark:rotate-0 dark:scale-100'
+        className='absolute rotate-90 scale-0 transition-all -translate-x-full
+          duration-500 dark:rotate-0 dark:scale-100 dark:translate-x-0'
       />
       <span className='sr-only'>Toggle theme</span>
     </Button>
