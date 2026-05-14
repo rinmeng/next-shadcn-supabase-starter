84a15012e35f5c0a745da2d7141d43386ad4cf10
rinmeng-gitbash <phurinmeng@gmail.com>
2026-05-14 04:07:57 -0700

    Update Logo component to include new icon and layout changes
    - Added ChevronRight icon to enhance visual hierarchy.
    - Updated links and images to reflect new branding.
    - Introduced new 8th_svg.svg icon file.

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui';
 import { cn } from '@/lib/utils';
-import { Infinity } from 'lucide-react';
+import { ChevronRight, Infinity } from 'lucide-react';
 
 interface LogoProps {
   className?: string;
@@ -34,15 +34,31 @@ export function Logo({ className, iconSize = 20, showText = true, onClick }: Log
 
       <Infinity size={iconSize} className={cn('text-muted-foreground', className)} />
       <Button variant='link' className={cn('p-0 m-0', className)}>
-        <Link href='https://rinm.dev'>
-          <Image
-            src='/rmlogov2.png'
-            alt='Logo'
-            width={Math.round(iconSize * 1.5)}
-            height={Math.round(iconSize * 1.5 * (1079 / 1905))}
-            style={{ width: iconSize * 1.5, height: 'auto' }}
+        <div className={cn('flex gap-2', className)}>
+          <Link href='https://web8th.com'>
+            <Image
+              src='/icons/8th_svg.svg'
+              alt='Logo'
+              width={Math.round(iconSize * 1.5)}
+              height={Math.round(iconSize * 1.5 * (1079 / 1905))}
+              style={{ width: iconSize * 1.5, height: 'auto' }}
+            />
+          </Link>
+
+          <ChevronRight
+            size={iconSize}
+            className={cn('text-muted-foreground', className)}
           />
-        </Link>
+          <Link href='https://rinm.dev'>
+            <Image
+              src='/rmlogov2.png'
+              alt='Logo'
+              width={Math.round(iconSize * 1.5)}
+              height={Math.round(iconSize * 1.5 * (1079 / 1905))}
+              style={{ width: iconSize * 1.5, height: 'auto' }}
+            />
+          </Link>
+        </div>
       </Button>
     </div>
   );
